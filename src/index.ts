9366fc6b7d242bb9dd941715e5c2d49100f29b82
Andrey Chechkin <andrey.chechkin@me.com>
2021-06-18 17:28:41 +0300

    Add mongoose and dotenv packages. Add connect to mongoDB then start app

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const PORT = process.env.PORT ?? 8080;
 
+//INIT APPS
 const app = express();
+dotenv.config();
+
+//USE MIDDLEWARES
 app.use(express.static(path.join(__dirname, '../', '/fintech-courses-fe/build')));
+
+//USE ROUTES
 app.get('/', (req, res) => {
 	res.sendFile('index.html');
 });
 
-app.listen(PORT, () => {
+//RUN APPLICATION
+if (process.env.DB_URL) {
+	mongoose.connect(process.env.DB_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+} else {
 	// eslint-disable-next-line no-console
-	console.log(`Server is running on http://localhost:${PORT}`);
+	console.error('Provide DB_URL for start application!');
+}
+
+const db = mongoose.connection;
+// eslint-disable-next-line no-console
+db.on('error', console.error.bind(console, 'DB connection error:'));
+db.once('open', function () {
+	app.listen(PORT, () => {
+		// eslint-disable-next-line no-console
+		console.log(`Server is running on http://localhost:${PORT}`);
+	});
 });
